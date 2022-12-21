90d57fb651d8c6507b23c9d32afc79b4f0d4337f
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2022-12-21 16:50:50 +0100

    debut css

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,19 +1,20 @@
 //boutton
 let button=document.querySelector("button")
 let main=document.getElementsByClassName("main")[0]
+let header=document.getElementsByClassName("header")[0]
 let h2=document.createElement("h2")
-main.appendChild(h2)
+h2.className="header__subtitle"
+header.appendChild(h2)
 
 //préparation de la météo
-let div_meteo=document.createElement("div")
-div_meteo.className="meteo"
-main.appendChild(div_meteo)
+let list_meteo=document.createElement("div")
+list_meteo.className="list_meteo"
+main.appendChild(list_meteo)
 
 
+//creation de la météo
 button.addEventListener("click",()=>{
-    while(div_meteo.firstChild){
-        div_meteo.removeChild(div_meteo.firstChild)
-    }
+    list_meteo.innerHTML=""
     let input=document.querySelector("input")
     city=input.value
     const fetchName = (city) => fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ city+"&cnt=8&units=metric&appid=1cdbc48903d1d699f34e221b79e38af7");
@@ -25,26 +26,29 @@ button.addEventListener("click",()=>{
         h2.textContent=data.city.name
         console.log(data)
         for (let elem of data.list){
+            let meteo=document.createElement("div")
+            meteo.className="meteo"
+            list_meteo.appendChild(meteo)
             let heure=document.createElement("p")
             heure.className="meteo__heure"
-            div_meteo.appendChild(heure)
-            heure.textContent=elem.dt_txt.split(" ")[1].substring(0,5);;
+            meteo.appendChild(heure)
+            heure.textContent=elem.dt_txt.split(" ")[1].substring(0,5);
             let logo=document.createElement("img")
             logo.className="meteo__logo "
-            div_meteo.appendChild(logo)
+            meteo.appendChild(logo)
             logo.src="http://openweathermap.org/img/wn/"+elem.weather[0].icon+"@2x.png"
             let description=document.createElement("p")
             description.className="meteo__description"
-            div_meteo.appendChild(description)
+            meteo.appendChild(description)
             description.textContent=elem.weather[0].description
             let temperature=document.createElement("p")
             temperature.className="meteo__temperature"
-            div_meteo.appendChild(temperature)
+            meteo.appendChild(temperature)
             temperature.textContent=(elem.main.temp).toFixed(0)+ " ° "
             let wind=document.createElement("p")
             wind.className="meteo__wind"
             wind.textContent=(elem.wind.speed*3.6).toFixed(0)+" km/h"
-            div_meteo.appendChild(wind)
+            meteo.appendChild(wind)
         }
     })
 })
