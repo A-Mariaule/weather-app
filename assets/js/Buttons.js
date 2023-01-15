1d71341eda451e0fcce7c3c73e17459402e1ea96
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2023-01-15 14:44:10 +0100

    modif

--- assets/js/Buttons.js
+++ assets/js/Buttons.js
@@ -77,10 +77,10 @@ export function newdate(date){
         return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+"0"+(date.getDate())
     }
     else if(date.getMonth()<10){
-        return date.getFullYear()+"-"+"0"+(date.getMonth()+1)+"-"(date.getDate())
+        return date.getFullYear()+"-"+"0"+(date.getMonth()+1)+"-"+(date.getDate())
     }
     else{
-        return date.getFullYear()+"-"+(date.getMonth()+1)+"-"(date.getDate())
+        return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDate())
     }
 }
 
